15d82e7aaacf2c6f40c86c191e5c8eec060fa41d
Nico Coetzee <nicc777@gmail.com>
2020-05-02 13:24:09 +0200

    Updated module documentation

--- app-src/cool_app/persistence/user_profiles.py
+++ app-src/cool_app/persistence/user_profiles.py
@@ -8,6 +8,10 @@ from cool_app.persistence import engine
 
 class User:
 
+    '''
+        The User class represents a user profile
+    '''
+
     def __init__(
         self,
         logger: ServiceLogger=ServiceLogger(),
@@ -21,6 +25,18 @@ class User:
         self.engine = engine
 
     def create_user_profile(self)->bool:
+        '''
+            The basic preparation steps you need to take before calling this function, that will create a new user profile, is:
+
+            >>> u = User()
+            >>> u.user_alias = 'FatCat'
+            >>> u.user_email_address = 'user1@example.tld'
+            >>> u.account_status = 1
+            >>> if create_user_profile() is True:
+            >>>     pass    # Handle success
+            >>> else:
+            >>>     pass    # Handle failure
+        '''
         user_created = True
         self.L.info(message='Attempting to create user profile in database for user with e-mail address "{}"'.format(self.user_email_address))
         if self.uid is None and self.user_alias is not None and self.user_email_address is not None and self.account_status is not None:
@@ -41,6 +57,17 @@ class User:
         return user_created
 
     def load_user_profile_by_email_address(self, user_email_address: str)->bool:
+        '''
+            Loads the user profile from the database based on the input email_address.
+
+            A successful retrieval will set the other properties of the use in this class
+
+            >>> u =User()
+            >>> if u.load_user_profile_by_email_address(user_email_address='user1@example.tld'):
+            >>>     pass    # Use the User object
+            >>> else:
+            >>>     pass    # Handle failure (profile did not load)
+        '''
         user_loaded = False
         self.uid = None
         self.user_email_address = None
@@ -65,6 +92,17 @@ class User:
         return user_loaded
 
     def load_user_profile_by_uid(self, uid: int)->bool:
+        '''
+            Loads the user profile from the database based on the input uid.
+
+            A successful retrieval will set the other properties of the use in this class
+
+            >>> u =User()
+            >>> if u.load_user_profile_by_uid(uid=123):
+            >>>     pass    # Use the User object
+            >>> else:
+            >>>     pass    # Handle failure (profile did not load)
+        '''
         user_loaded = False
         self.uid = None
         self.user_email_address = None
@@ -89,6 +127,19 @@ class User:
         return user_loaded
 
     def update_user_profile(self)->bool:
+        '''
+            Updates the user profile. Write out all properties to the database.
+
+            >>> u =User()
+            >>> if u.load_user_profile_by_uid(uid=123):
+            >>>     u.user_alias = 'SimplyTheBest'
+            >>>     if u.update_user_profile():
+            >>>         pass    # Use the updated User object with the new alias
+            >>>     else:
+            >>>         pass    # Handle failure (profile did not load)
+            >>> else:
+            >>>     pass    # Handle failure (profile did not load)
+        '''
         user_updated = False
         try:
             if self.engine is not None:
